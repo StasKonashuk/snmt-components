91d0fb75e7c4c828dcec59b6d8e105bfc719945e
Stas Konashuk <skonashuk@list.ru>
2023-10-26 13:36:52 +0300

    fix: Fix button

--- packages/button/src/button/button.tsx
+++ packages/button/src/button/button.tsx
@@ -63,6 +63,8 @@ const Button = (props: ButtonProps) => {
     setButtonClasses(newButtonClasses);
   }, [className, secondary]);
 
+  console.log("w");
+
   return (
     <div className={wrapperClassName ? wrapperClassName : ""}>
       <button
