c9f6581d6d00ba28efe86381aa6c45a29d4df0ab
Stas Konashuk <skonashuk@list.ru>
2023-10-31 15:53:29 +0300

    add test console

--- packages/button/src/button/button.tsx
+++ packages/button/src/button/button.tsx
@@ -63,6 +63,7 @@ const Button = (props: ButtonProps) => {
     setButtonClasses(newButtonClasses);
   }, [className, secondary]);
 
+  console.log("Button");
   return (
     <div className={wrapperClassName ? wrapperClassName : ""}>
       <button
