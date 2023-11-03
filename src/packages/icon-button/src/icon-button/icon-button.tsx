8432fb2fe25de1eaba73699fcd409bc72df5cfc6
Stas Konashuk <skonashuk@list.ru>
2023-11-03 17:23:58 +0300

    fix: Fix webpack configs

--- src/packages/icon-button/src/icon-button/icon-button.tsx
+++ src/packages/icon-button/src/icon-button/icon-button.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Button, ButtonProps } from "../../../button/src";
+//@ts-ignore
+import { Button, ButtonProps } from "snmt-button";
 
 import "./icon-button.css";
 
 interface IconButtonProps extends ButtonProps {}
 
 const IconButton = (props: IconButtonProps) => {
+  //@ts-ignore
   const { wrapperClassName, children, value, ...otherProps } = props;
 
   return (
